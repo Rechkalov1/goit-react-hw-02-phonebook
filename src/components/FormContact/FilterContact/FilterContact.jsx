ea4b9ada2bb6f58684ac9f828d0241af14db9b7d
EnVy <envy7321@gmail.com>
2022-09-19 17:04:12 +0300

    save

---
+++ src/components/FormContact/FilterContact/FilterContact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { nanoid } from "nanoid";
+import {Input} from '../FilterContact/FilterContact.styled'
+
+
+
+export const  FilterContact = ({handleChange})=>{
+    const filterId = nanoid();
+
+    return(
+        <Input id={filterId}
+                onChange={handleChange}
+                type="text"
+                name="filter"
+                placeholder="Find Contacts"
+              />
+    )
+ 
+   
+}
